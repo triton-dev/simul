67107ce392618ee1a953607dada5276baeec63d6
triton-dev <szikoragy@gmail.com>
2016-12-08 13:23:51 +0100

    Create eeprom.h

---
+++ eeprom.h
@@ -0,0 +1,36 @@
+/*
+	 EEPROM szimuláció
+
+	 Az arduino pro micro, mini lapka EEPROM elérését 
+	 szimulálja.
+
+*/
+
+#ifndef EEPROM_H
+#define EEPROM_H
+
+
+class EEPROM {
+
+	private:
+        const static int LEN = 512;
+        char mem[LEN];
+        
+	public:
+	// EEPROM törlése
+		bool clear();
+	// 1 bájt olvasása adott címről.
+		unsigned int read(unsigned int);
+	// 1 bájt írása adott címre.
+		bool write(unsigned int, unsigned char);
+  // EEPROM méretének lekérdezése
+		unsigned int length();
+	// Kontrsuktor
+		EEPROM();
+	// Destruktor
+		~EEPROM();
+
+
+};
+
+#endif
