2e8de4ad30886f704f3ff39feb565c4542e9645a
triton-dev <szikoragy@gmail.com>
2016-12-08 15:20:01 +0100

    Update lm35.cpp

--- lm35.cpp
+++ lm35.cpp
@@ -3,10 +3,21 @@
 
 LM35::LM35() {
   pin = 23;
+  digitalValue = 0;
 }
 
 LM35::LM35(int pin) {
   this pin = pin;
+  digitalValue = 0;
+}
+
+LM35::LM35(int pin, int val) {
+  this pin = pin;
+  if (val >=0 && val < 1024) {
+    digitalValue = val;
+  } else {
+   digitalValue = val % 1024; 
+  }
 }
 
 LM35::~LM35() {
@@ -14,20 +25,33 @@ LM35::~LM35() {
 }
 
 LM35::setPin(int pin) {
-  this pin = pin;
+  if (pin > 22 && pin < 29) {
+    this pin = pin;
+  } else {
+    pin = 23; 
+  }
 }
 
 LM35::getPin() {
+  return pin;
 }
 
-LM35::setAnalogValue(int) {
-
+LM35::setAnalogValue(int val) {
+  if (val >= 0 && val <= 5000) {
+    digitalValue = (1024 * val) / 5000;
+  } else {
+   digitalValue = 0; 
+  }
 }
 
-LM35::setDigitalValue(int) {
-
+LM35::setDigitalValue(int val) {
+  if (val >=0 && val <= 1023) {
+   digitalValue = val; 
+  } else {
+   digitalValue = val % 1024; 
+  }
 }
 
-LM35::getDigitalValue(int) {
-
+LM35::getDigitalValue(int val) {
+  return digitalValue;
 }
