b9e4e0e37eff21f354ada99179008d30b213fa20
triton-dev <szikoragy@gmail.com>
2016-12-08 14:58:01 +0100

    Create lm35.h

---
+++ lm35.h
@@ -0,0 +1,29 @@
+/*
+  LM35 hőmérő IC
+  
+  Virtulálisan beköthető egy analog csatlakozóra,
+  lekérdezhető Pin, konstruktorból beállítható Pin, alapértlemezett Pin A0 = 23
+  
+*/
+
+#ifndef LM35_H
+#define LM35_H
+
+class LM35 {
+  private:
+    int pin;
+    
+  public:
+    setPin(int);
+    getPin(int);
+    setAnalogValue(int);
+    setDigitalValue(int);
+    getDigitalValue(int);
+    
+    LM35();
+    LM35(int);
+    ~LM35();
+};
+
+
+#endif
